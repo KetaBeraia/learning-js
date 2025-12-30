ce65d4b2ea3dadde3eac167cec0c9d7a9cd986ec
KetaBeraia <keta.beraia4@gmail.com>
2025-12-30 22:01:21 +0400

    coinflip

--- projects/COINFLIP/coinflip.js
+++ projects/COINFLIP/coinflip.js
@@ -6,5 +6,5 @@ if (num > 0.5) {
   console.log("Tails");
 }
 
-// This is a simple coin flip program that generates a random number between 0 and 1.
+// This is a very simple coin flip program that generates a random number between 0 and 1.
 // If the number is greater than 0.5, it prints "Heads"; otherwise, it prints "Tails".
